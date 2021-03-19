994cf81b66cb3f5977703ffe638c269f93af2ea8
fzlilg980 <wuduanmang1972@126.com>
2021-03-19 13:47:25 +0800

    aa

---
+++ src/main/java/com/xh/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.xh.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.xh.entiy.Student;
+
+public interface StudentRepository extends CrudRepository<Student, Integer>{
+
+}
